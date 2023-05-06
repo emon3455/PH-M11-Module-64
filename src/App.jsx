31da0c202cb2dc49b43a7286f960827532b28a87
Emon <emon.mhk69@gmail.com>
2023-05-06 07:47:31 +0600

    data send to server side and display done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
 import './App.css'
+import { json } from 'react-router-dom';
 
 function App() {
 
@@ -13,14 +14,51 @@ function App() {
     .catch(er=>{
       console.log(er.message);
     })
-  },[])
+  },[]);
+
+  const handleSubmit = (e)=>{
+
+    e.preventDefault();
+    const form = e.target;
+    const name  = form.name.value;
+    const email = form.email.value;
+    const user={name ,email}
+
+
+    fetch(`http://localhost:5000/users`,{
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=> res.json())
+    .then(data=> {
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+
+      form.reset();
+    })
+    .catch(er=>{
+      console.log(er.message);
+    })
+
+  }
 
 
   return (
     <>
      
       <h1>Users Managements system</h1>
-      <p>Total users: {users.length}</p>
+      <p className='mb-1 '>Total users: {users.length}</p>
+      <form onSubmit={handleSubmit} className='p-1 bg-sky-light mb-1'>
+        <br />
+        <input type='text' name='name' id="name" required></input> 
+        <br />
+        <input type='email' name='email' id="email" required></input> 
+        <br />
+        <input type="submit" value="Submit" /> 
+      </form>
       <div className="">
         {
           users.map(user=> <p key={user.id}> {user.id} : {user.name} : {user.email} </p>)
